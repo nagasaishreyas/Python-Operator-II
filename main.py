9abc53695b39f5abca93b3ef9bb45ee0980b3b62
sudheerkuntumalla <sudheerkuntumalla3004@gmail.com>
2025-07-23 20:40:24 +0530

    s

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 a = 10
 b = 10
-if a > 0 and b > 0:
-    print("The numbers are greater than 0")
-     
+c = 12
+
+print(a != b)
+print(b != c)
+
+a = "python"
+b = "coding"
+
+if a != b:
+    print(a , 'and' , b , 'are different')
+    
+    
+    
+    
+    
+    
+    
+    a = 4
+    b = 5
+    
+    if (a == 1) != (b == 5):
+        print("Hello")
+        
+        
+        
+        a = int(input("Enter the number"))
+        
+        if a%2 != 0:
+            print(a, "is not even number")
+            
